49ca4771737d03a57046edb8357afe34d515e28e
Polina Khristenko <p.hristenko@runway-agency.ru>
2022-10-16 23:56:03 +0300

    wip: Main structure

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  $('.reviews__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            arrows: false,
+            dots: true
+          }
+        },
+    ]
+  });
+
+  const contactModal = $('.contact__modal');
+
+  $('.js-modal-btn').on("click", function() {
+    contactModal.fadeIn('fast');
+    contactModal.css('display', 'flex');
+    $('html').css('overflow', 'hidden');
+  });
+
+  $('.contact__modal, .contact__close').on("click", function (e) {
+    if (e.target == this) {
+      contactModal.fadeOut(300);
+      $('html').css('overflow', 'auto');
+      console.log('dfjdfj')
+    };
+  });
+
+
+  $('.education__title').click(function(){
+      $(this).toggleClass('active').next().slideToggle(300);
+  });
+});
